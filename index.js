8a82544b8c85219c88f3ec02dc61241bf28d8d2b
Melvin <jmelvin1134@gmail.com>
2023-03-26 19:49:12 -0400

    parameters

---
+++ index.js
@@ -0,0 +1,10 @@
+"before all"
+function logTwoValues(value1, value2) {
+    console.log(`The two values are ${value1} and ${value2}.`);
+}
+function sayHelloTo(firstName = "User") {
+        console.log(`Hello, ${firstName}!`);
+}
+      function sayHelloTo(Gracie) {
+        console.log(`"Hi, my name is ${Gracie} and I am learning to program in JavaScript."`) ;
+}
